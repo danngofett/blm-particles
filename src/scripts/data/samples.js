a8067070fcc47fbecdc376a73b83c3215b950cf2
Daniel Ngo <daniel@wemakewebsites.com>
2020-06-08 01:33:36 +1000

    BUILD: Updated content

--- src/scripts/data/samples.js
+++ src/scripts/data/samples.js
@@ -1,10 +1,21 @@
 const samples = [
   {
+    src: 'images/philando-castile.png',
+    body: {
+      age: 32,
+      causeOfDeath:
+        "Stopped and fatally shot seven times by police officer, Jeronimo Yanez, after reaching for his wallet when ordered to show his license and registration. Castile's partner, Diamond Reynolds live streamed the aftermath of the shooting on Facebook which sparked local and national protests.",
+      date: 'July 06, 2016',
+      name: 'Philando Castile',
+      state: 'Falcon Heights, Minnesota',
+    },
+  },
+  {
     src: 'images/sandra-bland.png',
     body: {
       age: 28,
       causeOfDeath:
-        'Found hanged in a jail cell three days after an altercation with a police officer, Brian Encinia, during a heated pretextual traffic stop and yelling “I will light you up. Get out. Now.”',
+        'Found hanged in a jail cell three days after being arrested due to an altercation with police officer, Brian Encinia. Bland was stopped under a pretextual traffic stop. Encinia was recorded yelling “I will light you up. Get out. Now.”',
       date: 'July 13, 2015',
       name: 'Sandra Bland',
       state: 'Waller County, Texas',
@@ -15,7 +26,7 @@ const samples = [
     body: {
       age: 27,
       causeOfDeath:
-        'Murdered by an NYPD officer, Daniel Pantaleo, after putting him in a chokehold whilst detaining him for suspicion of selling single cigarettes from packs without tax stamps. Garner repeated “I can\t breathe 11 times.”',
+        "Murdered by an NYPD officer, Daniel Pantaleo, after putting him in a chokehold whilst detaining him for suspicion of selling single cigarettes from packs without tax stamps. Garner repeated “I can't breathe 11 times.”",
       date: 'July 17, 2014',
       name: 'Eric Garner',
       state: 'Staten Island, New York',
@@ -26,7 +37,7 @@ const samples = [
     body: {
       age: 18,
       causeOfDeath:
-        'Fatally shot by 28-year-old white Ferguson police officer, Darren Wilson, after an altercation.',
+        "Fatally shot by Ferguson police officer, Darren Wilson, after firing 12 shots whilst Brown was unarmed and had his backed turned. Brown's death ignited the protests in Ferguson which also sparked international attention on police militarisation and the rallying cry “Hands up, don't shoot”.",
       date: 'August 9, 2014',
       name: 'Michael Brown',
       state: 'Ferguson, Missouri',
@@ -48,8 +59,8 @@ const samples = [
     body: {
       age: 17,
       causeOfDeath:
-        'Fatally shot by a lone vigilante, George Zimmerman, outside a convenience store after the perpetuator called 9-1-1 on suspicion and was immediately told not to follow the victim.',
-      date: '26 February 2020',
+        "Fatally shot by a lone vigilante, George Zimmerman, outside a convenience store after the perpetuator called 9-1-1 on a 'suspicion'. Zimmerman was immediately told not to follow the Martin by the dispatcher. Zimmerman pled not guilty under the Stand Your Ground law in Florida.",
+      date: '26 February 2012',
       name: 'Trayvon Martin',
       state: 'Sanford, Florida',
     },
@@ -59,7 +70,7 @@ const samples = [
     body: {
       age: 25,
       causeOfDeath:
-        'Murdered in cold blood by white supremacists whilst going for a jog.',
+        'Chased and murdered in cold blood by 3 white supremacists whilst going for a jog. The perpetrators were not arrested until 74 days later, after the video went viral, sparked debates on racial profiling in America.',
       date: '23 February 2020',
       name: 'Ahmaud Arbery',
       state: 'Brunswick, Georgia',
@@ -71,7 +82,7 @@ const samples = [
       age: 26,
       name: 'Breonna Taylor',
       causeOfDeath:
-        'Fatally shot during a gunfire exchange by police who used forced entry by battering ram in her own home.',
+        "Fatally shot during a gunfire exchange between Taylor's boyfriend and the police. The police used forced entry by battering ram into Taylor's own home under the suspicion of selling drugs 16km away. No drugs were found in the apartment.",
       date: 'March 13, 2020',
       state: 'Louisville, Kentucky',
     },
@@ -82,7 +93,7 @@ const samples = [
       age: 46,
       name: 'George Floyd',
       causeOfDeath:
-        'Murdered by asphyxiation after having his neck knelt on by a police officer, Derek Chauvin, for nine minutes whilst handcuffed facedown on the street by the police.',
+        'Murdered by asphyxiation after having his neck knelt on by a police officer, Derek Chauvin, for nine minutes whilst handcuffed facedown on the street. 3 other officers were also present during the murder.',
       date: 'May 25, 2020',
       state: 'Minneapolis, Minnesota',
     },
