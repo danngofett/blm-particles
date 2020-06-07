a1f65472e412a3eed8cab33ea3b2475357e6b62e
Daniel Ngo <daniel@wemakewebsites.com>
2020-06-07 22:20:36 +1000

    BUILD: Updated base styling and added content

---
+++ src/scripts/data/samples.js
@@ -0,0 +1,58 @@
+const samples = [
+  {
+    src: 'images/tamir-rice.png',
+    body: {
+      age: 12,
+      causeOfDeath:
+        'Fatally shot by a police officer, Timothy Loehmann, for holding a toy gun. 9-1-1 responder repeatedly asked if the boy was black or white.',
+      date: '22 November 2014',
+      name: 'Tamir Rice',
+      state: 'Cleveland, Ohio',
+    },
+  },
+  {
+    src: 'images/trayvon-martin.png',
+    body: {
+      age: 17,
+      causeOfDeath:
+        'Fatally shot by a lone vigilante, George Zimmerman, outside a convenience store after the perpetuator called 9-1-1 on suspicion and was immediately told not to follow the victim.',
+      date: '26 February 2020',
+      name: 'Trayvon Martin',
+      state: 'Sanford, Florida',
+    },
+  },
+  {
+    src: 'images/ahmaud-arbery.png',
+    body: {
+      age: 25,
+      causeOfDeath:
+        'Murdered in cold blood by white supremacists whilst going for a jog.',
+      date: '23 February 2020',
+      name: 'Ahmaud Arbery',
+      state: 'Brunswick, Georgia',
+    },
+  },
+  {
+    src: 'images/breonna-taylor.png',
+    body: {
+      age: 26,
+      name: 'Breonna Taylor',
+      causeOfDeath: 'Fatally shot by police in her own home by forced entry.',
+      date: 'March 13, 2020',
+      state: 'Louisville, Kentucky',
+    },
+  },
+  {
+    src: 'images/george-floyd.png',
+    body: {
+      age: 46,
+      name: 'George Floyd',
+      causeOfDeath:
+        'Murdered by asphyxiation after having his neck knelt on by a police office, Derek Chauvin, for nine minutes whilst handcuffed facedown on the street by the police.',
+      date: 'May 25, 2020',
+      state: 'Minneapolis, Minnesota',
+    },
+  },
+];
+
+export default samples;
--- src/scripts/webgl/WebGLView.js
+++ src/scripts/webgl/WebGLView.js
@@ -3,100 +3,123 @@ import { TweenLite } from 'gsap/TweenMax';
 
 import InteractiveControls from './controls/InteractiveControls';
 import Particles from './particles/Particles';
+import samples from './../data/samples';
 
 const glslify = require('glslify');
 
 export default class WebGLView {
+  constructor(app) {
+    this.app = app;
 
-	constructor(app) {
-		this.app = app;
+    this.samples = samples;
 
-		this.samples = [
-			'images/trayvon-martin.png',
-			'images/ahmaud-arbery.png',
-			'images/breona-taylor.png',
-			'images/george-floyd.png',
-		];
+    this.nameSelector = document.querySelector('[js-view="name"]');
+    this.ageSelector = document.querySelector('[js-view="age"]');
+    this.stateSelector = document.querySelector('[js-view="state"]');
+    this.dateSelector = document.querySelector('[js-view="date"]');
+    this.causeOfDeathSelector = document.querySelector(
+      '[js-view="cause-of-death"]',
+    );
 
-		this.initThree();
-		this.initParticles();
-		this.initControls();
+    this.initThree();
+    this.initParticles();
+    this.initControls();
 
-		const rnd = ~~(Math.random() * this.samples.length);
-		this.goto(rnd);
-	}
+    const rnd = ~~(Math.random() * this.samples.length);
+    this.goto(rnd);
+  }
 
-	initThree() {
-		// scene
-		this.scene = new THREE.Scene();
+  initThree() {
+    // scene
+    this.scene = new THREE.Scene();
 
-		// camera
-		this.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
-		this.camera.position.z = 300;
+    // camera
+    this.camera = new THREE.PerspectiveCamera(
+      50,
+      window.innerWidth / window.innerHeight,
+      1,
+      10000,
+    );
+    this.camera.position.z = 300;
 
-		// renderer
-        this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    // renderer
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 
-        // clock
-		this.clock = new THREE.Clock(true);
-	}
+    // clock
+    this.clock = new THREE.Clock(true);
+  }
 
-	initControls() {
-		this.interactive = new InteractiveControls(this.camera, this.renderer.domElement);
-	}
+  initControls() {
+    this.interactive = new InteractiveControls(
+      this.camera,
+      this.renderer.domElement,
+    );
+  }
 
-	initParticles() {
-		this.particles = new Particles(this);
-		this.scene.add(this.particles.container);
-	}
+  initParticles() {
+    this.particles = new Particles(this);
+    this.scene.add(this.particles.container);
+  }
 
-	// ---------------------------------------------------------------------------------------------
-	// PUBLIC
-	// ---------------------------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------------------------
+  // PUBLIC
+  // ---------------------------------------------------------------------------------------------
 
-	update() {
-		const delta = this.clock.getDelta();
+  update() {
+    const delta = this.clock.getDelta();
 
-		if (this.particles) this.particles.update(delta);
-	}
+    if (this.particles) this.particles.update(delta);
+  }
 
-	draw() {
-		this.renderer.render(this.scene, this.camera);
-	}
+  draw() {
+    this.renderer.render(this.scene, this.camera);
+  }
 
+  goto(index) {
+    if (this.currentSample == null) {
+      this.particles.init(this.samples[index].src);
+      this.updateContent(index);
+    } else {
+      this.particles.hide(true).then(() => {
+        this.particles.init(this.samples[index].src);
+        this.updateContent(index);
+      });
+    }
 
-	goto(index) {
-		// init next
-		if (this.currSample == null) this.particles.init(this.samples[index]);
-		// hide curr then init next
-		else {
-			this.particles.hide(true).then(() => {
-				this.particles.init(this.samples[index]);
-			});
-		}
+    this.currentSample = index;
+  }
 
-		this.currSample = index;
-	}
+  updateContent(index) {
+    this.nameSelector.innerText = this.samples[index].body.name;
+    this.ageSelector.innerText = this.samples[index].body.age;
+    this.dateSelector.innerText = this.samples[index].body.date;
+    this.stateSelector.innerText = this.samples[index].body.state;
+    this.causeOfDeathSelector.innerText = this.samples[index].body.causeOfDeath;
+  }
 
-	next() {
-		if (this.currSample < this.samples.length - 1) this.goto(this.currSample + 1);
-		else this.goto(0);
-	}
+  next() {
+    if (this.currentSample < this.samples.length - 1)
+      this.goto(this.currentSample + 1);
+    else this.goto(0);
+  }
 
-	// ---------------------------------------------------------------------------------------------
-	// EVENT HANDLERS
-	// ---------------------------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------------------------
+  // EVENT HANDLERS
+  // ---------------------------------------------------------------------------------------------
 
-	resize() {
-		if (!this.renderer) return;
-		this.camera.aspect = window.innerWidth / window.innerHeight;
-		this.camera.updateProjectionMatrix();
+  resize() {
+    if (!this.renderer) return;
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
 
-		this.fovHeight = 2 * Math.tan((this.camera.fov * Math.PI) / 180 / 2) * this.camera.position.z;
+    this.fovHeight =
+      2 *
+      Math.tan((this.camera.fov * Math.PI) / 180 / 2) *
+      this.camera.position.z;
 
-		this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-		if (this.interactive) this.interactive.resize();
-		if (this.particles) this.particles.resize();
-	}
+    if (this.interactive) this.interactive.resize();
+    if (this.particles) this.particles.resize();
+  }
 }
