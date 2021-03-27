00a140de33d56aada2c1898daced0fb024917942
Daniel Ngo <hello@danielno.com>
2021-03-27 17:52:04 +1100

    chore: removed rayshard brooks

--- src/scripts/data/samples.js
+++ src/scripts/data/samples.js
@@ -1,147 +1,136 @@
 const samples = [
   {
-    src: 'images/freddie-gray.png',
+    src: "images/freddie-gray.png",
     body: {
       age: 25,
       causeOfDeath:
         "Spinal injury after being beaten, and handcuffed in a police van where he was taken for a 'rough ride'. A form of police brutality where victims are left unbelted and frequently thrown around in the vans due to eratic driving. All 5 officers involved were not charged of any crimes.",
-      date: 'April 19, 2015',
-      name: 'Freddie Gray',
-      state: 'Baltimore, Maryland',
+      date: "April 19, 2015",
+      name: "Freddie Gray",
+      state: "Baltimore, Maryland",
     },
   },
   {
-    src: 'images/john-crawford.png',
+    src: "images/john-crawford.png",
     body: {
       age: 22,
       causeOfDeath:
         "Shot to death at a Walmart whilst on his cell phone while holding a BB gun. Ohio is an 'open carry' state which allows open carrying of firearms. Neither of the officers involved were charged.",
-      date: 'August 05, 2014',
-      name: 'John Crawford III',
-      state: 'Beavercreek, Ohio',
+      date: "August 05, 2014",
+      name: "John Crawford III",
+      state: "Beavercreek, Ohio",
     },
   },
   {
-    src: 'images/philando-castile.png',
+    src: "images/philando-castile.png",
     body: {
       age: 32,
       causeOfDeath:
-        'Stopped for a busted tail light and fatally shot seven times by police officer, Jeronimo Yanez, after reaching for his wallet when ordered to show his license and registration.',
-      date: 'July 06, 2016',
-      name: 'Philando Castile',
-      state: 'Falcon Heights, Minnesota',
+        "Stopped for a busted tail light and fatally shot seven times by police officer, Jeronimo Yanez, after reaching for his wallet when ordered to show his license and registration.",
+      date: "July 06, 2016",
+      name: "Philando Castile",
+      state: "Falcon Heights, Minnesota",
     },
   },
   {
-    src: 'images/sandra-bland.png',
+    src: "images/sandra-bland.png",
     body: {
       age: 28,
       causeOfDeath:
-        'Found hanged in a jail cell three days after being arrested due to an altercation with police officer, Brian Encinia. Bland was stopped under a pretextual traffic stop. Encinia was recorded yelling “I will light you up. Get out. Now.”',
-      date: 'July 13, 2015',
-      name: 'Sandra Bland',
-      state: 'Waller County, Texas',
+        "Found hanged in a jail cell three days after being arrested due to an altercation with police officer, Brian Encinia. Bland was stopped under a pretextual traffic stop. Encinia was recorded yelling “I will light you up. Get out. Now.”",
+      date: "July 13, 2015",
+      name: "Sandra Bland",
+      state: "Waller County, Texas",
     },
   },
   {
-    src: 'images/eric-garner.png',
+    src: "images/eric-garner.png",
     body: {
       age: 27,
       causeOfDeath:
         "Murdered by an NYPD officer, Daniel Pantaleo, after putting him in a chokehold whilst detaining him for suspicion of selling single cigarettes from packs without tax stamps. Garner was heard saying “Every time you see me, you want to mess with me. I'm tired of it. It stops today.” and repeated “I can't breathe 11 times.” moments before his death.",
-      date: 'July 17, 2014',
-      name: 'Eric Garner',
-      state: 'Staten Island, New York',
+      date: "July 17, 2014",
+      name: "Eric Garner",
+      state: "Staten Island, New York",
     },
   },
   {
-    src: 'images/michael-brown.png',
+    src: "images/michael-brown.png",
     body: {
       age: 18,
       causeOfDeath:
         "Fatally shot by Ferguson police officer, Darren Wilson, after firing 12 shots whilst Brown was unarmed and had his backed turned. Brown's death ignited the protests in Ferguson which also sparked international attention on police militarisation and the rallying cry “Hands up, don't shoot”.",
-      date: 'August 9, 2014',
-      name: 'Michael Brown',
-      state: 'Ferguson, Missouri',
+      date: "August 9, 2014",
+      name: "Michael Brown",
+      state: "Ferguson, Missouri",
     },
   },
   {
-    src: 'images/tamir-rice.png',
+    src: "images/tamir-rice.png",
     body: {
       age: 12,
       causeOfDeath:
-        'Fatally shot by a police officer, Timothy Loehmann, for holding a toy gun. 9-1-1 responder repeatedly asked if the boy was black or white.',
-      date: '22 November 2014',
-      name: 'Tamir Rice',
-      state: 'Cleveland, Ohio',
+        "Fatally shot by a police officer, Timothy Loehmann, for holding a toy gun. 9-1-1 responder repeatedly asked if the boy was black or white.",
+      date: "22 November 2014",
+      name: "Tamir Rice",
+      state: "Cleveland, Ohio",
     },
   },
   {
-    src: 'images/trayvon-martin.png',
+    src: "images/trayvon-martin.png",
     body: {
       age: 17,
       causeOfDeath:
         "Fatally shot by a lone vigilante, George Zimmerman, outside a convenience store after the perpetuator called 9-1-1 on a 'suspicion'. Zimmerman was immediately told not to follow the Martin by the dispatcher. Zimmerman pled not guilty under the Stand Your Ground law in Florida.",
-      date: '26 February 2012',
-      name: 'Trayvon Martin',
-      state: 'Sanford, Florida',
+      date: "26 February 2012",
+      name: "Trayvon Martin",
+      state: "Sanford, Florida",
     },
   },
   {
-    src: 'images/ahmaud-arbery.png',
+    src: "images/ahmaud-arbery.png",
     body: {
       age: 25,
       causeOfDeath:
-        'Chased and murdered in cold blood by 3 white supremacists whilst going for a jog. The perpetrators were not arrested until 74 days later, after the video went viral, sparked debates on racial profiling in America.',
-      date: '23 February 2020',
-      name: 'Ahmaud Arbery',
-      state: 'Brunswick, Georgia',
+        "Chased and murdered in cold blood by 3 white supremacists whilst going for a jog. The perpetrators were not arrested until 74 days later, after the video went viral, sparked debates on racial profiling in America.",
+      date: "23 February 2020",
+      name: "Ahmaud Arbery",
+      state: "Brunswick, Georgia",
     },
   },
   {
-    src: 'images/breonna-taylor.png',
+    src: "images/breonna-taylor.png",
     body: {
       age: 26,
-      name: 'Breonna Taylor',
+      name: "Breonna Taylor",
       causeOfDeath:
         "Fatally shot during a gunfire exchange between Taylor's boyfriend and the police. The police used forced entry by battering ram into Taylor's own home under the suspicion of selling drugs 16km away. No drugs were found in the apartment.",
-      date: 'March 13, 2020',
-      state: 'Louisville, Kentucky',
+      date: "March 13, 2020",
+      state: "Louisville, Kentucky",
     },
   },
   {
-    src: 'images/george-floyd.png',
+    src: "images/george-floyd.png",
     body: {
       age: 46,
-      name: 'George Floyd',
+      name: "George Floyd",
       causeOfDeath:
-        'Murdered by asphyxiation after having his neck knelt on by a police officer, Derek Chauvin, for 8 minutes and 46 seconds whilst handcuffed facedown on the street. 3 other officers were also present during the murder.',
-      date: 'May 25, 2020',
-      state: 'Minneapolis, Minnesota',
+        "Murdered by asphyxiation after having his neck knelt on by a police officer, Derek Chauvin, for 8 minutes and 46 seconds whilst handcuffed facedown on the street. 3 other officers were also present during the murder.",
+      date: "May 25, 2020",
+      state: "Minneapolis, Minnesota",
     },
   },
   {
-    src: 'images/rayshard-brooks.png',
-    body: {
-      age: 27,
-      name: 'Rayshard Brooks',
-      causeOfDeath:
-        'Murdered by Atlanta Police Department officer, Garrett Rolfe, for sleeping in his car and blocking a drive-through lane.',
-      date: 'June 12, 2020',
-      state: 'Atlanta, Georgia',
-    }
-  },
-  {
-    src: 'images/jacob-blake.png',
+    src: "images/jacob-blake.png",
     body: {
       age: 29,
-      name: 'Jacob Blake',
+      name: "Jacob Blake",
       causeOfDeath:
-        'Was fatally shot 7 seven times, by Rusten Sheskey, during an arrest. Three of Blake\'s sons were in the backseat.',
-      date: 'August 23, 2020',
-      state: 'Kenosha, Wisconsin',
-    }
-  }
+        "Was fatally shot 7 seven times, by Rusten Sheskey, during an arrest. Three of Blake's sons were in the backseat.",
+      date: "August 23, 2020",
+      state: "Kenosha, Wisconsin",
+    },
+  },
 ];
 
 export default samples;
